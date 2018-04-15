292d847e4c5b33c4fb7dca3a4abb198a0289acf2
Patryk Wiśniewski <patryk.wisniewski@outlook.com>
2018-04-15 22:47:26 +0200

    working on scheduling algorithm

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Team from "../models/Team";
 
 const styles = {
   fontFamily: "sans-serif",
@@ -12,49 +13,41 @@ export class Schedule extends React.Component {
   };
 
   componentWillReceiveProps(nextProps) {
-    this.createSchedule();
-    this.setState({ teams: nextProps.teams });
-  }
-
-  createSchedule = () => {
-    let { teams } = this.state;
-    let matches = [];
-
-    // game + revange
-    // teams.forEach(team => {
-    //   team.opponents.forEach(o => {
-    //     matches.push({
-    //       id: matches.length + 1,
-    //       team1: team.name,
-    //       team2: o.name
-    //     });
-    //   });
-    // });
-
-    teams.forEach(team => {
-      team.opponents.forEach(o => {
-        var obj = matches.find(function(obj) {
-          return (
-            (obj.team1 === o.name || obj.team1 === team.name) &&
-            (obj.team2 === o.name || obj.team2 === team.name)
-          );
-        });
-
-        if (!obj) {
-          matches.push({
-            id: matches.length + 1,
-            team1: team.name,
-            team2: o.name,
-            team1Score: "",
-            team2Score: "",
-            resultIsSet: false
-          });
-        }
-      });
-    });
+    let total;
+    if (this.props.teams.length % 2 === 0) {
+      total = this.props.teams.length - 1;
+    } else {
+      total = this.props.teams.length;
+    }
 
     this.setState({
-      games: matches
+      teams: nextProps.teams,
+      total: total
+    });
+
+    // alert('willReceive')
+
+    this.creategames();
+  }
+
+  creategames = () => {
+    // let { teams } = this.state;
+
+    let teams = Array.apply(0, Array(this.state.teams.length)).map(
+      (_, i) => new Team({ id: i + 1 })
+    );
+
+    let fixtures = [];
+    for (let f = 0; f < teams.length - 1; f++) {
+      let pp = teams.slice(),
+        games = [],
+        count = Math.floor(pp.length / 2);
+      while (count--) games.push([pp.shift(), pp.pop()]);
+      fixtures.push({ id: f + 1, games });
+      teams = [new Team({ id: 1 })].concat(teams.slice(2), [teams[1]]);
+    }
+    this.setState({
+      fixtures
     });
   };
 
@@ -65,17 +58,25 @@ export class Schedule extends React.Component {
   }
 
   render() {
-    let { games, teams } = this.state;
+    let { fixtures, teams, total, gamesbyFixture } = this.state;
     return (
       <div style={styles}>
         {teams.length > 0 && (
           <div>
             {" "}
-            <h2>Schedule</h2>
+            <h2>fixtures</h2>
             <ul>
-              {games.map(e => (
-                <li key={e.id}>
-                  game {e.id}: {e.team1} vs {e.team2}{" "}
+              {fixtures.map(f => (
+                <li key={f.id}>
+                  fixture no {f.id}
+                  <ol>
+                    {f.games.map(g => (
+                      <li key={g.id}>
+                        {" "}
+                        {g[0].id} vs {g[1].id}{" "}
+                      </li>
+                    ))}
+                  </ol>
                 </li>
               ))}
             </ul>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
-import Hello from "./Hello";
 import Team from "./models/Team";
 import { Schedule } from "./components/Schedule";
 
@@ -12,7 +11,7 @@ const styles = {
 class App extends React.Component {
   state = {
     numOfTeams: 4,
-    teamHasChoosen: false,
+    submitted: false,
     teams: []
   };
 
@@ -40,7 +39,7 @@ class App extends React.Component {
       return {
         ...prevState,
         teams: [...prevState.teams],
-        teamHasChoosen: true
+        submitted: true
       };
     });
 
@@ -48,23 +47,21 @@ class App extends React.Component {
   };
 
   render() {
-    let { teams, teamHasChoosen } = this.state;
+    let { teams, submitted } = this.state;
     return (
       <div style={styles}>
-        <Hello name="CodeSandbox" />
-
-        {!teamHasChoosen && (
+        <h1>Fixtures generator</h1>
+        {!submitted && (
           <form onSubmit={this.handleSubmit}>
             <label>How many teams? </label>
 
             <select value={this.state.numOfTeams} onChange={this.handleChange}>
-              <option value="2">2</option>
               <option value="4">4</option>
+              <option value="5">5</option>
               <option value="6">6</option>
-              <option value="8">8</option>
             </select>
 
-            <input type="submit" value="Submit" />
+            <input type="submit" value="Create a schedule" />
           </form>
         )}
         <Schedule teams={teams} />
